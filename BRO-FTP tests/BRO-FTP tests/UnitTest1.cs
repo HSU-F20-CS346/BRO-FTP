50c5c4fd722d714f7d954478c36220c88c8a7c76
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2020-10-15 14:08:41 -0700

    Update UnitTest1.cs

--- BRO-FTP tests/BRO-FTP tests/UnitTest1.cs
+++ BRO-FTP tests/BRO-FTP tests/UnitTest1.cs
@@ -6,11 +6,18 @@ namespace BRO_FTP_tests
     {
 
         [Test]
-        public void ConnectionTest()
+        public void ConnectionTestSuccess()
+        {
+            
+        }
+
+
+        [Test]
+        public void ConnectionTestFail()
         {
 
         }
-       
+
         [Test]
         public void SenderTest()
         {
@@ -24,9 +31,23 @@ namespace BRO_FTP_tests
         }
 
         [Test]
-        public void FileListingTest()
+        public void FileIntegrityCheck()
+        {
+            //Checking to make sure the file sent is the same as the file received
+        }
+
+        [Test]
+        public void FileListingTestSuccess()
         {
 
         }
+
+
+        [Test]
+        public void FileListingTestFail()
+        {
+
+        }
+
     }
 }
