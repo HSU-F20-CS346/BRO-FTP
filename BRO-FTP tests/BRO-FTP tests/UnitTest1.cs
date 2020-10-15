a637f917e6ea207a558148f8cc6e22114ad70b0e
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2020-10-15 13:54:58 -0700

    Basical tests

---
+++ BRO-FTP tests/BRO-FTP tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace BRO_FTP_tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void ConnectionTest()
+        {
+
+        }
+       
+        [Test]
+        public void SenderTest()
+        {
+           
+        }
+
+        [Test]
+        public void ReceiverTest()
+        {
+
+        }
+
+        [Test]
+        public void FileListingTest()
+        {
+
+        }
+    }
+}
