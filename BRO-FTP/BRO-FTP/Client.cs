22a96b8f30b81de905bb710c1006482c2043f104
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2020-10-27 20:39:48 -0700

    Update Client.cs
    Small fixes

--- BRO-FTP/BRO-FTP/Client.cs
+++ BRO-FTP/BRO-FTP/Client.cs
@@ -8,6 +8,52 @@ using System.Threading;
 namespace BRO_FTP
 {    class Listener
     {
+
+        static void TCPListener()
+        {
+            TcpListener server = null;
+
+            // creates a TCP listener on local IP and waiting for a connection to be made
+            try
+            {
+                // Local IP and port to listen on
+                int port = 4000;
+                IPAddress localAddr = IPAddress.Parse("local ip"); //Needs your IP to function
+
+                // new TCP listener creation
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for connections
+                server.Start();
+
+                // Enter the listening loop waiting for connection
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // look for client to accept
+                    TcpClient client = server.AcceptTcpClient();
+
+                    if (client.Connected)
+                    {
+                        Console.WriteLine("Connection from secondary!");
+                        break;
+                    }
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+        }
+
         // Pings an IP and port trying to connect
         static void Connect(String IPAddress, String portNum) 
         {
@@ -48,52 +94,14 @@ namespace BRO_FTP
             Thread thread = new Thread(() =>
             {
                 Thread.Sleep(5000);
-                Connect("192.168.1.108", "13000");
+                TCPListener();
             });
             thread.Start();
 
-
+            Connect("Target IP", "4000"); // Needs target IP to function
+          
             
-            TcpListener server = null;
-
-            // creates a TCP listener on local IP and waiting for a connection to be made
-            try
-            {
-                // Local IP and port to listen on
-                int port = 13000;
-                IPAddress localAddr = IPAddress.Parse("192.168.1.100");
-
-                // new TCP listener creation
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for connections
-                server.Start();
-
-                // Enter the listening loop waiting for connection
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // look for client to accept
-                    TcpClient client = server.AcceptTcpClient();
-
-                    if (client.Connected)
-                    {
-                        Console.WriteLine("Connection from secondary!");
-                        break;
-                    }
-
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
+           
         }
     }
 }
